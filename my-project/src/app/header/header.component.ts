4f58111515083b1bc517e4bc38b5c22177b0977b
mohamed-mostafa-ragab <you@example.com>
2023-08-30 01:33:32 +0200

    first commit

---
+++ my-project/src/app/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+content:string='Our Content'
+s:string='subscription'
+action:string='Our official Website'
+}
